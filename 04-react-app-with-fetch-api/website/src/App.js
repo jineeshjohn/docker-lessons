f0b3b97d0c8d6507a91a361772ddcfa57f295d9b
jjohn <jjohn@virtusa.com>
2019-05-21 19:58:09 +0100

    Create a react app fetching a external api

---
+++ 04-react-app-with-fetch-api/website/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState({ hits: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://hn.algolia.com/api/v1/search?query=redux',
+      );
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox!!</h1>
+      <h2>Start editing to see some!!!</h2>
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM node:7
-WORKDIR /app
-COPY package.json /app
-RUN npm install
-COPY index.js /app
